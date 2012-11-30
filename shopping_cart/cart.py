24e1854d7e273daf50ee44f06f6c136d71fb9ff0
Greg Nofi <nofinator@yahoo.com>
2012-11-30 02:16:10 -0500

    Fix bug: products from separate merchants should have separate carts.

--- shopping_cart/cart.py
+++ shopping_cart/cart.py
@@ -3,13 +3,13 @@ import datetime
 import shopping_cart.models as models
 
 
-CART_ID = 'CART-ID'
+CART_ID_FORMAT = '%s-CART-ID'
 
 
 class Cart(object):
 
     def __init__(self, request):
-        cart_id = request.session.get(CART_ID)
+        cart_id = request.session.get(CART_ID_FORMAT % request.subdomain)
         if cart_id:
             try:
                 cart = models.Cart.objects.get(id=cart_id,
@@ -27,7 +27,7 @@ class Cart(object):
     def new(self, request):
         cart = models.Cart()
         cart.save()
-        request.session[CART_ID] = cart.id
+        request.session[CART_ID_FORMAT % request.subdomain] = cart.id
         return cart
 
     def add(self, product):
